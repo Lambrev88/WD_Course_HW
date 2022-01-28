173961bec020e643e3514c6e879fed9b8e03ff5f
Lambrev88 <92799271+Lambrev88@users.noreply.github.com>
2022-01-28 14:20:48 +0200

    HW1 Done

---
+++ hw1/js/findMaxEven_Task.js
@@ -0,0 +1,26 @@
+// TASK: да се дефинира функцията findMaxEven(), която да връща максималното четно число от подаденият й масив.
+
+// ---> YOUR CODE HERE <---
+
+function findMaxEven(arr) {
+        let maxnum = arr[0];
+
+        for (let i = 0; i < arr.length; i++) {
+
+                if ((arr[i] % 2 === 0) && (arr[i] >= maxnum)) {
+                        maxnum = arr[i];
+                }
+
+        }
+        return  maxnum;
+}
+
+
+
+
+
+
+// TEST
+let numbers = [2, -4, 5, 3, 9, 0, 1];
+let max = findMaxEven(numbers);
+console.log(`The max of ${numbers} is ${max}`);
